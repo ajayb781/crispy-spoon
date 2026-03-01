2a9fd520b9b43e8cf297d085bdcc40ce483fdfec
ajayb781 <ajay07bhatia@gmail.com>
2026-03-02 01:12:45 +0530

    Adding a simple Java file

---
+++ ajay1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ajay1{
+    public static void main(String args[]){
+        String name;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please Enter Your Name");
+        name = in.nextLine();
+        System.out.println("Your Name is " +name);
+    }
+}
